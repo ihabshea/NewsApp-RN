9a4bae92615b4494d0ef9aa4400ad4aa69dde695
Ihab E. McShea <ihabshea@gmail.com>
2021-04-30 21:35:57 +0200

    perf: use of react native screens
    optimizing memory usage and performance for navigation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import MainNavigation from './src/navigation';
 import { store } from './src/stores';
 import { Provider } from 'react-redux';
+import { enableScreens } from 'react-native-screens';
+enableScreens();
 const App = () => {
   return (
     <Provider store={store}>
