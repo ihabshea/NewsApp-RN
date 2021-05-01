65f7e549d95d526aa2b26ec68811105dbe967a74
Ihab E. McShea <ihabshea@gmail.com>
2021-05-01 12:26:14 +0200

    fix: fix return type of API call

--- src/API/News/getTopHeadlines.ts
+++ src/API/News/getTopHeadlines.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import {API_URL, API_KEY} from '@env';
-import { NewsResponse, TopNewsFilter } from "../../types";
+import {NewsResponse, TopNewsFilter} from '../../types';
 
-export default async function getTopHeadlines(filter: TopNewsFilter): Promise<() => NewsResponse> {
-    const {country, category} =  filter;
-    const url = category?
-    `${API_URL}/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}`
-    :`${API_URL}/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
-    const response = await axios.get(url);
-    // console.log(response)
-    return response.data;
+export default async function getTopHeadlines(
+  filter: TopNewsFilter,
+): Promise<NewsResponse> {
+  const {country, category} = filter;
+  const url = category
+    ? `${API_URL}/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}`
+    : `${API_URL}/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
+  const response = await axios.get(url);
+  return response.data;
 }
